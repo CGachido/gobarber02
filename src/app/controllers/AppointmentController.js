2d36244ffedb03a17ecf57d80b487aab9cc5b831
Cassius Sanches Gachido de Souza <cassius.sanches@hotmail.com>
2019-10-11 13:40:52 -0300

    Sending email after cancelation

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,6 +5,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -125,8 +126,9 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
-
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [{ model: User, as: 'provider', attributes: ['name', 'email'] }],
+    });
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: 'You dont have permission to cancel this appointment',
@@ -144,6 +146,11 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+    await Mail.sendEmail({
+      to: `${appointment.provider.name} > ${appointment.provider.email}>`,
+      subject: 'Schedule canceled',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
