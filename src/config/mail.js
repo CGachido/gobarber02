14e872f9289dc35a17a09089ba0dc4627859ca0f
Cassius Sanches Gachido de Souza <cassius.sanches@hotmail.com>
2019-10-11 13:24:40 -0300

    Creating nodemailer config and send mail service

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8468e10abced52',
+    password: '9d4d2689ffad3b',
+  },
+  default: {
+    from: 'Go Barber <noreply@gobarber.com.br>',
+  },
+};
